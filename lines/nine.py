26e97530849bfe7436a62d8449d91de004a29983
Jamie Kellogg <jbkellogg@users.noreply.github.com>
2024-02-27 17:19:57 +0000

    new file:   lines/.cs50.yml 	new file:   lines/__init__.py 	new file:   lines/five.py 	new file:   lines/four.py 	new file:   lines/invalid_extension.txt 	new file:   lines/nine.py 	new file:   lines/three.py 	new file:   lines/two-thousand-fifty-eight.py

---
+++ lines/nine.py
@@ -0,0 +1,20 @@
+"""
+This file prints Hello World and This is CS50
+"""
+
+def main():
+    """main function"""
+
+    # Print Hello World
+    print("Hello, world")
+
+    # Print This is CS50
+    print("This is CS50")
+
+    print("hello again")
+    print("obla di")
+    print("obla dah")
+    print("life goes on, yah!")
+
+if __name__ == "__main__":
+    main()
